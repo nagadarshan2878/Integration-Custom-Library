8ea030b99243de14e0749277269fae0634cda6da
Nandhakumar <nandhakumar.muthusamy@jmrinfotech.com>
2025-03-06 15:48:07 +0530

    integration

--- src/ShareDataService.ts
+++ src/ShareDataService.ts
@@ -1,20 +1,16 @@
 export class ShareDataService {
     private storageKey: string = "appToken";
-  
     setToken(token: string): void {
       if (typeof window !== "undefined" && window.sessionStorage) {
         window.sessionStorage.setItem(this.storageKey, token);
       }
     }
-  
     getToken(): string | null {
       return window.sessionStorage.getItem(this.storageKey);
     }
-
     setCookieToken(token: string): void {
         document.cookie = `${this.storageKey}=${token}; path=/; secure; samesite=strict`;
       }
-      
     getCookieToken(): string | null {
         const cookies = document.cookie.split("; ");
         for (const cookie of cookies) {
